ff6486a4d8e93b5db1b84e6cebe2f2154a91b09f
Chernikov Leonid <leonidcernikov43@gmail.com>
2026-09-17 23:06:44 +0300

    split_ranges

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
-#include<iostream>
+#include <iostream>
 #include <chrono>
+#include <vector>
 
 using data_t = std::vector<unsigned long long>;
 using value_t = data_t::value_type;
 
-void howDivVec(int** vec,int* size, int* threads_count){
-    int whole = 0;
-    int rem = 0;
-    whole = *size / *threads_count;
-    rem = *size % *threads_count;
+std::vector<std::pair<data_t::const_iterator, data_t::const_iterator>>
+split_ranges(const data_t& data, size_t threads_count)
+{
+    std::vector<std::pair<data_t::const_iterator, data_t::const_iterator>> ranges;
+    size_t whole = data.size() / threads_count;
+    size_t rem = data.size() % threads_count;
 
+    auto current = data.begin();
+
+    for (size_t i = 0; i < threads_count; i++) {
+        size_t chunk_size = (i < rem) ? whole + 1 : whole;
+        auto next = current + chunk_size;
+        ranges.push_back({current, next});
+        current = next;
+    }
+    return ranges;
 }
+
 value_t sum_range(data_t::const_iterator begin, data_t::const_iterator end)
 {
     value_t sum = 0;
